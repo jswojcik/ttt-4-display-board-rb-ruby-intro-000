752e628d3862d9864c61e474ccb0cb7c05beea72
Jacob Wojcik <jswojcik9@gmail.com>
2020-04-08 05:42:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,9 +5,9 @@ def display_board(board)
 separator = "|"
 lines = "-----------"
 
-puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
-puts "#{lines}"
-puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
-puts "#{lines}"
-puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+print" #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+      "#{lines}"
+      " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+      "#{lines}"
+     " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
